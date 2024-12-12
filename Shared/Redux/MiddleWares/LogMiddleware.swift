daa1a7d544e673982e4f9b5e9014605f2304585b
Yuan Cao <ycao13@jaguarlandrover.com>
2024-12-12 15:00:53 +0800

    add log

--- Shared/Redux/MiddleWares/LogMiddleware.swift
+++ Shared/Redux/MiddleWares/LogMiddleware.swift
@@ -10,5 +10,9 @@ import Foundation
 func logMiddleware() -> Middleware<AppState> {
     return { state, action, dispatcher in
         print("👉\("Log Middleware")👈")
+        print("[\(DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .medium))] [\((#file as NSString).lastPathComponent):\(#line)] [\(#function)] - 👉\("sp")👈")
+        
+        print("[\(DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .medium))] [\((#file as NSString).lastPathComponent):\(#line)] [\(#function)] - 👉\("sdfds")👈")
+        
     }
  }
